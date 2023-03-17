433ccf30df5b5a8aff487fc3878a1594c1a49204
jecointr <104733115+jecointr@users.noreply.github.com>
2023-03-17 19:05:41 +0100

    Update minishell.h

--- includes/minishell.h
+++ includes/minishell.h
@@ -1,3 +1,23 @@
 /* Langage de programmation : C */
 
 #ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "libft.h"
+
+# include <errno.h>
+# include <fcntl.h>
+# include <limits.h>
+# include <signal.h
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/wait.h>
+# include <unistd.h>
+
+# define SUCCESS 0
+# define B 1
+# define C 126
+# define D 127
+# define E 4096
+  
+#endif
